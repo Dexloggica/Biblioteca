65b3c5a1319af39a33ee29deacd207c52a12bc18
Dex <42595658+Dexloggica@users.noreply.github.com>
2020-04-02 12:17:48 -0300

    Modificacion
    Se crea un archivo aparte para conectar a la base de datos, para hacer el alta baja y modificacion de manera mas ordenada.

--- Biblioteca.py
+++ Biblioteca.py
@@ -1,97 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #hola
-#Biblioteca por Dex Loggica 
-from tkinter import *
-import pymysql
-from math import *
-from tkinter import ttk
+#Biblioteca por Dex Loggica
 import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox as mb
 from tkinter import scrolledtext as st
+#from tkinter import *
+import pymysql
+#from math import *
+import conexion_base_datos
 
 class Application(ttk.Frame):
     def __init__(self, main_window):
         super().__init__(main_window)
         main_window.title("Biblioteca")
+
+        self.conexion=conexion_base_datos.Libros
         # Crear el panel de pestañas.
         self.notebook = ttk.Notebook(self)
         # Crear el contenido de cada una de las pestañas.
         self.carga_libros()
-        
-        self.busqueda_libros()
+        self.listado_completo()
+        #self.modificar()
+        self.borrado()
+
         self.notebook.pack(padx=10, pady=10)
         self.pack()
-    def conectar(self):
-        connection = pymysql.connect(host='localhost',user='root',password='', db='20181020_version1')
-        return connection
 
-    def registrar(self):
-        #recupero los datos dentro de los campos de texto
-        print("registrar")
-        titulo=self.titulo.get()
-        print(titulo)
-        print(type(titulo))
-        numero=self.numero.get()
-        print(numero)
-        print(type(numero))
-        paginas=self.paginas.get()
-        print(paginas)
-        print(type(paginas))
-        fecha=self.fecha.get()
-        print(fecha)
-        print(type(fecha))
-        #fecha="2012-12-01"
-        isbn=self.isbn.get()
-        print(isbn)
-        print(type(isbn))
-        imagen=self.imagen.get()
-        print(imagen)
-        print(type(imagen))
-        link=self.link.get()
-        print(link)
-        print(type(link))
-        #pais=IntVar()
-        pais=1
-        print(pais)
-        print(type(pais))
-       # editorial=IntVar()
-        editorial=1
-        print(editorial)
-        print(type(editorial))
-       # cantidad_pedidas=IntVar()
-        cantidad_pedidas=0
-        print(cantidad_pedidas)
-        print(type(cantidad_pedidas))
-        #0 esta 1 no esta en la biblioteca
-       # estado=IntVar()
-        estado=0
-        print(estado)
-        print(type(estado))
-        
-        #♥titulo=self.titulo.get()
-        #numero=self.tomo.get()
-        #paginas=self.cantidad.get()
-        #abro la conexion
-        connect = self.conectar()
-        #creo el puntero
-        cursor=connect.cursor()
-        #realizar consulta sql
-        print("entrando a consulta")
-        #sql="INSERT INTO libro (Titulo,Numero,Paginas,ISBN,LinkImagen,LinkDescarga,Pais_idPais,Editorial_idEditorial,CantidadVecesPedidas,Estado) VALUES ('%s','%d','%d','%s','%s','%s','%d','%d','%d','%d')" % (titulo,numero,paginas,isbn,imagen,link,pais,editorial,cantidad_pedidas,estado)
-        sql="INSERT INTO libro (Titulo,Numero,Paginas,FechaPublicacion,ISBN,LinkImagen,LinkDescarga,Pais_idPais,Editorial_idEditorial,CantidadVecesPedidas,Estado) VALUES ('%s','%d','%d','%s','%s','%s','%s','%d','%d','%d','%d')" % (titulo,numero,paginas,fecha,isbn,imagen,link,pais,editorial,cantidad_pedidas,estado)
-        try:
-            print("try")
-            cursor.execute(sql)
-            #cerrar puntero
-            connect.commit()
-        except:
-            print("except")
-            #en el caso de error
-            connect.rollback()
-        cursor.close()
-        connect.close()
+    
 
     def carga_libros(self):
+        #Este es el formulario para cargar libros
         self.pagina1 = ttk.Frame(self.notebook)
         self.notebook.add(self.pagina1, text="Carga de libros")
         self.labelframe1=ttk.LabelFrame(self.pagina1, text="Libro")
@@ -141,19 +81,70 @@ class Application(ttk.Frame):
         
         
         #Boton para confirmar registro
-        self.boton1=ttk.Button(self.labelframe1, text="Registrar",command=self.registrar)
+        self.boton1=ttk.Button(self.labelframe1, text="Registrar",command=self.agregar)
         self.boton1.grid(column=0, row=7, padx=4, pady=4, columnspan=2)
 
-    def busqueda_libros(self):
+    def agregar(self):
+        #recupero los datos dentro de los campos de texto del formulario de carga_libros()
+        print("registrar")
+
+        #estas variables hay que editarlas en el formulario para tener un select por codigos numericos
+        #no deberian ir fijas
+        pais=1
+        editorial=1
+        cantidad_pedidas=0
+        estado=0
+        
+        #####
+        datos=(self.titulo.get(), self.numero.get(), self.paginas.get(), self.fecha.get(), self.isbn.get(), self.imagen.get(), self.link.get(), pais, editorial, cantidad_pedidas, estado )
+        ######
+        self.conexion.alta(datos)
+        mb.showinfo("Información", "Los datos fueron cargados")
+        self.titulo.set("")
+        self.numero.set("")
+        self.paginas.set("")
+        self.fecha.set("")
+        self.isbn.set("")
+        self.imagen.set("") 
+        self.link.set("")
+        pais
+        editorial
+        cantidad_pedidas
+        estado
+
+    def consulta_por_codigo(self):
+        self.pagina2 = ttk.Frame(self.cuaderno1)
+        self.cuaderno1.add(self.pagina2, text="Consulta por código")
+        self.labelframe1=ttk.LabelFrame(self.pagina2, text="Artículo")
+        self.labelframe1.grid(column=0, row=0, padx=5, pady=10)
+        self.label1=ttk.Label(self.labelframe1, text="Código:")
+        self.label1.grid(column=0, row=0, padx=4, pady=4)
+        self.codigo=tk.StringVar()
+        self.entrycodigo=ttk.Entry(self.labelframe1, textvariable=self.codigo)
+        self.entrycodigo.grid(column=1, row=0, padx=4, pady=4)
+        self.label2=ttk.Label(self.labelframe1, text="Descripción:")        
+        self.label2.grid(column=0, row=1, padx=4, pady=4)
+        self.descripcion=tk.StringVar()
+        self.entrydescripcion=ttk.Entry(self.labelframe1, textvariable=self.descripcion, state="readonly")
+        self.entrydescripcion.grid(column=1, row=1, padx=4, pady=4)
+        self.label3=ttk.Label(self.labelframe1, text="Precio:")        
+        self.label3.grid(column=0, row=2, padx=4, pady=4)
+        self.precio=tk.StringVar()
+        self.entryprecio=ttk.Entry(self.labelframe1, textvariable=self.precio, state="readonly")
+        self.entryprecio.grid(column=1, row=2, padx=4, pady=4)
+        self.boton1=ttk.Button(self.labelframe1, text="Consultar", command=self.consultar)
+        self.boton1.grid(column=1, row=3, padx=4, pady=4)
+
+    def listado_completo(self):
         self.pagina1 = ttk.Frame(self.notebook)
-        self.notebook.add(self.pagina1, text="Busqueda de libros")
+        self.notebook.add(self.pagina1, text="Listado completo")
         self.labelframe1=ttk.LabelFrame(self.pagina1, text="Busqueda")
         self.labelframe1.grid(column=0, row=0, padx=5, pady=10)
         self.boton1=ttk.Button(self.labelframe1, text="Listado completo", command=self.listar)
         self.boton1.grid(column=0, row=0, padx=4, pady=4)
         self.scrolledtext1=st.ScrolledText(self.labelframe1, width=30, height=10)
         self.scrolledtext1.grid(column=0,row=1, padx=10, pady=10)
-        
+
     def listar(self):
         connect = self.conectar()
         cursor=connect.cursor()
@@ -165,6 +156,39 @@ class Application(ttk.Frame):
         cursor.close()
         connect.commit()
         connect.close()
+
+    def consultar(self):
+        datos=(self.codigo.get(), )
+        respuesta=self.articulo1.consulta(datos)
+        if len(respuesta)>0:
+            self.descripcion.set(respuesta[0][0])
+            self.precio.set(respuesta[0][1])
+        else:
+            self.descripcion.set('')
+            self.precio.set('')
+            mb.showinfo("Información", "No existe un artículo con dicho código")
+
+    def borrado(self):
+        self.pagina4 = ttk.Frame(self.cuaderno1)
+        self.cuaderno1.add(self.pagina4, text="Borrado de artículos")
+        self.labelframe1=ttk.LabelFrame(self.pagina4, text="Artículo")        
+        self.labelframe1.grid(column=0, row=0, padx=5, pady=10)
+        self.label1=ttk.Label(self.labelframe1, text="Código:")
+        self.label1.grid(column=0, row=0, padx=4, pady=4)
+        self.codigoborra=tk.StringVar()
+        self.entryborra=ttk.Entry(self.labelframe1, textvariable=self.codigoborra)
+        self.entryborra.grid(column=1, row=0, padx=4, pady=4)
+        self.boton1=ttk.Button(self.labelframe1, text="Borrar", command=self.borrar)
+        self.boton1.grid(column=1, row=1, padx=4, pady=4)
+
+    def borrar(self):
+        datos=(self.codigoborra.get(), )
+        cantidad=self.articulo1.baja(datos)
+        if cantidad==1:
+            mb.showinfo("Información", "Se borró el artículo con dicho código")
+        else:
+            mb.showinfo("Información", "No existe un artículo con dicho código")
+
 main_window = tk.Tk()
 app = Application(main_window)
 app.mainloop()
---
+++ conexion_base_datos.py
@@ -0,0 +1,64 @@
+import pymysql
+
+class Libros:
+
+    #conecto a la base de datos
+    def abrir(self):
+        connection = pymysql.connect(host='localhost',user='root',password='', db='20181020_version1')
+        return connection
+        
+
+    def alta(self, datos):
+        connect = self.abrir()
+        #creo el puntero
+        cursor=connect.cursor()
+        #realizar consulta sql
+        print("entrando a consulta")
+        #sql="INSERT INTO libro (Titulo,Numero,Paginas,ISBN,LinkImagen,LinkDescarga,Pais_idPais,Editorial_idEditorial,CantidadVecesPedidas,Estado) VALUES ('%s','%d','%d','%s','%s','%s','%d','%d','%d','%d')" % (titulo,numero,paginas,isbn,imagen,link,pais,editorial,cantidad_pedidas,estado)
+        sql="INSERT INTO libro (Titulo,Numero,Paginas,FechaPublicacion,ISBN,LinkImagen,LinkDescarga,Pais_idPais,Editorial_idEditorial,CantidadVecesPedidas,Estado) VALUES ('%s','%d','%d','%s','%s','%s','%s','%d','%d','%d','%d')" % (titulo,numero,paginas,fecha,isbn,imagen,link,pais,editorial,cantidad_pedidas,estado)
+        try:
+            print("try")
+            cursor.execute(sql)
+            #cerrar puntero
+            connect.commit()
+        except:
+            print("except")
+            #en el caso de error
+            connect.rollback()
+        cursor.close()
+        connect.close()
+
+
+    def consulta(self, datos):
+        cone=self.abrir()
+        cursor=cone.cursor()
+        sql="select descripcion, precio from articulos where codigo=%s"
+        cursor.execute(sql, datos)
+        cone.close()
+        return cursor.fetchall()
+
+    def recuperar_todos(self):
+        cone=self.abrir()
+        cursor=cone.cursor()
+        sql="select codigo, descripcion, precio from articulos"
+        cursor.execute(sql)
+        cone.close()
+        return cursor.fetchall()
+
+    def baja(self, datos):
+        cone=self.abrir()
+        cursor=cone.cursor()
+        sql="delete from articulos where codigo=%s"
+        cursor.execute(sql, datos)
+        cone.commit()
+        cone.close()
+        return cursor.rowcount # retornamos la cantidad de filas borradas
+
+    def modificacion(self, datos):
+        cone=self.abrir()
+        cursor=cone.cursor()
+        sql="update articulos set descripcion=%s, precio=%s where codigo=%s"
+        cursor.execute(sql, datos)
+        cone.commit()
+        cone.close()
+        return cursor.rowcount # retornamos la cantidad de filas modificadas
